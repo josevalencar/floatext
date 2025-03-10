5fd90d8dac596b393f48fee0ae0bf33a1ddf56e3
josevalencar <jose.silva@sou.inteli.edu.br>
2025-03-10 11:16:37 -0300

    Initial Commit

---
+++ Floatext/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  Floatext
+//
+//  Created by José Vitor Alencar on 10/03/25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ Floatext/FloatextApp.swift
@@ -0,0 +1,33 @@
+//
+//  FloatextApp.swift
+//  Floatext
+//
+//  Created by José Vitor Alencar on 10/03/25.
+//
+
+import SwiftUI
+
+@main
+struct FloatextApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
